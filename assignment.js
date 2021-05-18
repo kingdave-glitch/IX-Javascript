4c10900f9b24d907fdee608d83c798658055bae0
mbaya david <davidmbaya01@gmail.com>
2021-05-18 15:18:10 +0100

    Third commit

---
+++ assignment.js
@@ -0,0 +1,26 @@
+var a;
+var b;
+var c;
+
+//string
+a = 'David'
+console.log(a)
+console.log(typeof(a))
+
+//integers
+a = 50
+console.log(a)
+console.log(typeof(a))
+
+//float
+a = 100.01
+console.log(a)
+console.log((a).toFixed (2))
+console.log(typeof(a))
+
+//boolean
+a = 70
+b = 90
+c = a < b
+console.log(c)
+console.log(typeof(c))
