8e93544c2a7d8a231ec37e2048d49fbb204a1359
mbaya david <davidmbaya01@gmail.com>
2021-05-18 10:41:42 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var a;//declaration
+var c;
+var d;
+a = 5;//assignment
+d = 10;
+console.log(a);//printing to console
+
+var b = 'this is javascript';//declaration and assignment
+
+console.log(b);
+
+c = 100;
+console.log(c);
+
+function add (){
+    var e = 20
+    var f = 30
+    console.log(a+d)
+}
