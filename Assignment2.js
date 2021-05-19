438910bf68554ccbc4a0ec29d155af9028e79faf
mbaya david <davidmbaya01@gmail.com>
2021-05-19 13:33:23 +0100

    Fifth commit

---
+++ Assignment2.js
@@ -0,0 +1,12 @@
+var a = 10
+var b = 50
+if (a > b){
+    console.log("a is greater than b")
+} else if (a < b){
+    console.log("a is less than b") 
+} 
+if (a > 18){
+    console.log('a can be admitted into the university')
+} else if (a < 18){
+    console.log('a is rejected from the university')
+}
