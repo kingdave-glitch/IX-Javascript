ca7b32b73079dfe96087cb5dca43307775725ddb
mbaya david <davidmbaya01@gmail.com>
2021-05-19 12:31:55 +0100

    fourth commit

---
+++ class.js
@@ -0,0 +1,22 @@
+//console.log(5 < 5);
+
+// NO
+
+// x and y | result
+// true and true | true
+// true and false | false
+// false and false | false
+// false and true | false 
+
+var age = 15;
+var gender = 'm'
+
+
+if (age > 19 & gender == 'm') {
+    console.log("you can join the game cause you are  "+ age +" years old")
+} else if (age < 18 & age > 10) {
+    console.log("You can play ps3 at home")
+}
+else {
+    console.log("You can go and play at home")
+}
